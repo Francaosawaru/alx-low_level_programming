b3b927c0234da16572cb0c1984c30caca5aafc8a
Francaosawaru <forierie@yahoo.com>
2023-08-15 18:33:59 -0700

    Nature made the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - list all the natural numbers
+* Return: always 0
+ */
+int main(void)
+{
+int i. sum = 0;
+for (i = 0; i < 10024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum + = i;
+}
+printf("%d\n", sum);
+return (0);
+}
