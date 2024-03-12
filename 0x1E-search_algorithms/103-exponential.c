84b574943b8a226737013aac27511e07f15172ba
Francaosawaru <forierie@yahoo.com>
2024-03-12 13:45:27 -0700

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,88 +1,70 @@
 #include "search_algos.h"
 
 /**
- * repeat_search - uses recursion to find a value
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- *
- * Return: index of the number  or -1 if not found or arr empty
- */
-int repeat_search(int *array, size_t size, int value)
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
+int _binary_search(int *array, size_t left, size_t right, int value)
 {
-	size_t mid = size / 2;
 	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	printf("\n");
-
-	if (mid && size % 2 == 0)
-		mid--;
-	if (value == array[mid])
-		return ((int)mid);
-	if (value < array[mid])
-		return (repeat_search(array, mid, value));
-	mid++;
-
-	return (repeat_search(array + mid, size - mid, value) + mid);
-}
-
-/**
- * bin_search - calls binary search on an array
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- *
- * Return: index of the number or -1 if null or not found
- */
-int bin_search(int *array, size_t size, int value)
-{
-	int idx;
-
-	idx = repeat_search(array, size, value);
-	if (idx >= 0 && array[idx] != value)
-		return (-1);
-
-	return (idx);
-}
-
-/**
- * exponential_search - searches for a value in an array using its algorithm
- * @array: input array to seearch
- * @size: size of the array
- * @value: value to search the array for
- *
- * Return: index of the number or -1 if no found or null
- */
-int exponential_search(int *array, size_t size, int value)
-{
-	size_t idx, next;
-	int result;
 
 	if (array == NULL)
 		return (-1);
 
-	if (array[0] == value)
-		return (0);
-
-	idx = 1;
-	while (array[idx] < value && idx < size)
+	while (right >= left)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)idx, array[idx]);
-		idx *= 2;
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
 	}
 
-	next = (idx >= size) ? (size - 1) : idx;
-	idx /= 2;
-	printf("Value found between indexes [%d] and [%d]\n", (int)idx, (int)next);
-	result = bin_search(array + idx, (next + 1) - idx, value);
-	if (result >= 0)
-		result += idx;
+	return (-1);
+}
 
-	return (result);
+/**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0, right;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+	return (_binary_search(array, i / 2, right, value));
 }
